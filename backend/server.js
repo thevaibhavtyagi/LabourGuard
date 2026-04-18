210dafb6ebd3d78385f3136063b10983a7e80409
codesarthaksingh <sarthaklucknow32@gmail.com>
2026-04-18 11:00:59 +0530

    fix: employee dashboard

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import workRoutes from './routes/workRoutes.js';
